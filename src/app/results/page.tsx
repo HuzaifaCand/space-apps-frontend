80d66a07c9881ba51480c5973551375cee5636e2
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-10-05 18:51:57 +0500

    Acc fixed scn

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,19 +1,18 @@
 "use client";
 
 import Stars from "@/components/Stars";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import clsx from "clsx";
 import WeatherCard from "@/components/results/WeatherCard";
+import { getLocal } from "@/utils/storage";
+import { toast } from "sonner";
 import { useRouter } from "next/navigation";
-import { InfoIcon } from "lucide-react";
-import RecommendationPage from "@/components/results/RecommendationPage";
-import FinalPage from "@/components/results/FinalPage";
+type tabs = "Summary" | "Recommendation" | "Data";
 
-type tabs = "Summary" | "Recommendation" | "Data" | "Trends";
-const tabsList = ["Summary", "Recommendation", "Data", "Trends"] as tabs[];
+const tabsList = ["Summary", "Recommendation", "Data"] as tabs[];
 const baseClass = "py-2 rounded-lg text-xs sm:text-sm font-medium";
 
-type WeatherType =
+type stuff =
   | "Temperature"
   | "Relative_Humidity"
   | "Precipitation"
@@ -21,73 +20,56 @@ type WeatherType =
   | "Wind_Speed";
 
 export interface WeatherResults {
-  Predictions: {
-    [key in WeatherType]: {
-      Probability: number;
-      Status: string;
-      Distribution: Record<string, number>;
+  finalStats: {
+    Predictions: {
+      [key in stuff]: {
+        Probability: number;
+        Status: string;
+        Distribution: Record<string, number>;
+      };
     };
   };
 }
 
 export default function ResultsPage() {
+  const [results, setResults] = useState<WeatherResults>();
   const router = useRouter();
   const [activeTab, setActiveTab] = useState<tabs>("Summary");
 
-  const predictions: WeatherResults = {
-    Predictions: {
-      Precipitation: {
-        Probability: 0.12,
-        Status: "Low",
-        Distribution: { Low: 0.12, Moderate: 0.01, High: 0.01 },
-      },
-      Temperature: {
-        Probability: 0.82,
-        Status: "Mild",
-        Distribution: {
-          "Very Hot": 0.0,
-          Hot: 0.0,
-          Warm: 0.07,
-          Mild: 0.82,
-          Cool: 0.0,
-          Cold: 0.0,
-          "Very Cold": 0.0,
-        },
-      },
-      Wind_Speed: {
-        Probability: 0.66,
-        Status: "Calm",
-        Distribution: {
-          Calm: 0.66,
-          Breezy: 0.34,
-          Windy: 0.0,
-          "Very Windy": 0.0,
-        },
-      },
-      Relative_Humidity: {
-        Probability: 0.84,
-        Status: "Humid",
-        Distribution: {
-          Comfortable: 0.13,
-          Humid: 0.84,
-          "Very Uncomfortable": 0.03,
-        },
-      },
-      Heat_Index: {
-        Probability: 0.75,
-        Status: "Caution",
-        Distribution: {
-          Safe: 0.01,
-          Caution: 0.75,
-          "Extreme Caution": 0.23,
-          Danger: 0.0,
-          "Extreme Danger": 0.0,
-        },
-      },
-    },
-  };
+  useEffect(() => {
+    const loadResults = () => {
+      const data = getLocal("results");
 
-  const temperatureStatus = predictions.Predictions.Temperature.Status;
+      if (!data) {
+        toast.error(
+          "No results found. Please select your date and activity again."
+        );
+        router.replace("/date-and-activity");
+        return null;
+      }
+
+      if (typeof data.finalStats === "string") {
+        try {
+          data.finalStats = JSON.parse(data.finalStats);
+          console.log("Parsed finalStats:", data.finalStats);
+          console.log("Please be json", typeof data.finalStats);
+        } catch (err) {
+          console.error("Failed to parse finalStats:", err);
+          return null;
+        }
+      }
+
+      return data;
+    };
+
+    const storedData = loadResults();
+    if (storedData) setResults(storedData);
+  }, [router]);
+
+  if (!results) return null;
+
+  const predictions = results.finalStats.Predictions;
+  const temperatureStatus = predictions.Temperature.Status;
   const showHeatIndex =
     temperatureStatus === "Hot" ||
     temperatureStatus === "Warm" ||
@@ -97,82 +79,62 @@ export default function ResultsPage() {
     <>
       <Stars />
       <section className="flex flex-col gap-4 max-w-7xl mx-auto py-12 px-4">
-        {/* Tabs */}
-        <div className="w-full pb-2 pt-3 rounded-t-2xl px-6 no-scrollbar overflow-x-auto bg-background/80">
+        <div className=" w-full pb-2 pt-3 rounded-t-2xl px-6 no-scrollbar overflow-x-auto bg-background/80">
           <div className="flex gap-6 sm:gap-10 whitespace-nowrap">
-            {tabsList.map((t) => (
-              <button
-                key={t}
-                onClick={() => setActiveTab(t)}
-                className={clsx(
-                  baseClass,
-                  "relative hover:text-white",
-                  "after:absolute after:left-0 after:text-white after:-bottom-1.5 after:h-[0.5px] after:w-full after:bg-highlight after:scale-x-0 after:origin-center after:transition-transform after:duration-300",
-                  activeTab === t
-                    ? "text-white after:scale-x-100"
-                    : "text-white/70"
-                )}
-              >
-                {t}
-              </button>
-            ))}
+            {tabsList.map((t) => {
+              return (
+                <button
+                  key={t}
+                  onClick={() => setActiveTab(t)}
+                  className={clsx(
+                    baseClass,
+                    "relative hover:text-white",
+                    "after:absolute after:left-0 after:text-white after:-bottom-1.5 after:h-[0.5px] after:w-full after:bg-highlight after:scale-x-0 after:origin-center after:transition-transform after:duration-300",
+                    activeTab === t
+                      ? "text-white after:scale-x-100"
+                      : "text-white/70"
+                  )}
+                >
+                  {t}
+                </button>
+              );
+            })}
           </div>
         </div>
 
-        {/* Tab Content */}
         <div className="w-full py-12 bg-background/80 min-h-120 px-12 rounded-b-2xl">
           {activeTab === "Summary" && (
             <div className="grid gap-4 grid-cols-[repeat(auto-fit,minmax(300px,1fr))]">
               <WeatherCard
                 title="Temperature"
-                status={predictions.Predictions.Temperature.Status}
-                probability={predictions.Predictions.Temperature.Probability}
+                status={predictions.Temperature.Status}
+                probability={predictions.Temperature.Probability}
               />
               <WeatherCard
                 title="Humidity"
-                status={predictions.Predictions.Relative_Humidity.Status}
-                probability={
-                  predictions.Predictions.Relative_Humidity.Probability
-                }
+                status={predictions.Relative_Humidity.Status}
+                probability={predictions.Heat_Index.Probability}
               />
               <WeatherCard
                 title="Rainfall"
-                status={predictions.Predictions.Precipitation.Status}
-                probability={predictions.Predictions.Precipitation.Probability}
+                status={predictions.Precipitation.Status}
+                probability={predictions.Precipitation.Probability}
               />
               <WeatherCard
                 title="Wind Speed"
-                status={predictions.Predictions.Wind_Speed.Status}
-                probability={predictions.Predictions.Wind_Speed.Probability}
+                status={predictions.Wind_Speed.Status}
+                probability={predictions.Wind_Speed.Probability}
               />
               {showHeatIndex && (
                 <WeatherCard
                   title="Heat Index"
-                  status={predictions.Predictions.Heat_Index.Status}
-                  probability={predictions.Predictions.Heat_Index.Probability}
+                  status={predictions.Heat_Index.Status}
+                  probability={predictions.Heat_Index.Probability}
                 />
               )}
-
-              {/* Info Card */}
-              <div className="relative rounded-3xl p-6 sm:p-8 text-center text-white flex flex-col items-center justify-start bg-bg-background border-2 border-blue-500/50">
-                <div className="mb-4 mt-2">
-                  <InfoIcon size={48} className="text-blue-400/80" />
-                </div>
-                <h3 className="text-sm sm:text-md uppercase tracking-wide font-medium opacity-70 mb-2">
-                  About These Cards
-                </h3>
-                <p className="text-center text-[10px] sm:text-xs text-muted leading-snug max-w-[220px] mx-auto">
-                  Each card shows a weather condition with its likely status
-                  based on historical trends. The probability shows how often
-                  this condition has occurred on this date over the years. Use
-                  this to understand typical patterns and see what’s most likely
-                  on this day.
-                </p>
-              </div>
             </div>
           )}
-          {activeTab === "Recommendation" && <RecommendationPage />}
-          {activeTab === "Data" && <FinalPage />}
+          {/* {activeTab === "Recommendation" && <RecommendationPage />} */}
         </div>
       </section>
     </>
